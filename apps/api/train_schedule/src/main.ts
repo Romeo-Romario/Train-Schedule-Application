3e5d9673f47a6511e06abdf867eaa75d51659c03
Roman Krit <romankrit2005@gmail.com>
2025-04-19 12:41:19 +0300

    check

--- apps/api/train_schedule/src/main.ts
+++ apps/api/train_schedule/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
+  
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
   app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
